849b4f7fe26e5ad3f692264f3e6cd4d7e5e87363
jiao <dxlcq@outlook.com>
2024-01-09 19:52:17 +0800

    更新名字

--- Leg11/include/MainWindow.h
+++ Leg11/include/MainWindow.h
@@ -1,11 +1,6 @@
 #pragma once
 
 #include <QMainWindow>
-#include <QWidget>
-#include <QLabel>
-#include <QLineEdit>
-#include <QPushButton>
-#include <QPainter>
 
 class MainWindow : public QMainWindow{
     Q_OBJECT
@@ -13,12 +8,5 @@ public:
     MainWindow(QWidget *parent = nullptr); 
     ~MainWindow();
 private:
-    QWidget     _centralWidget;
-    QLabel      _A, _B;
-    QLineEdit   _lineEdit_A_X,
-                _lineEdit_A_Y,
-                _lineEdit_B_X,
-                _lineEdit_B_Y;
 
-    void paintEvent(QPaintEvent *event);
 };
